03ae4d03be96fefef66a3ed90bd473cf51489dfe
Dedaldino Papelo <dedaldino.papelo@okayulatech.ao>
2024-06-24 10:42:00 +0100

    FIX: change connection string to mongodb running on docker

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,7 @@ import mongoose from 'mongoose'
 
 export class mongoDB {
     connect() {
-        //mongodb://localhost:27017/pets_app
-        mongoose.connect('mongodb://admin:dev@mobile.com@mongodb:27017/petsApp').then(() => {
+        mongoose.connect('mongodb://deda:mypwd@localhost:27017/pets').then(() => {
             console.log("Connected")
         }).catch((error) => {
             console.log(error)
